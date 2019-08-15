cd54bf7d623e85d357512064dfa027e124ee982a
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-15 13:16:33 -0400

    form works

--- pages/admin/new-post/index.vue
+++ pages/admin/new-post/index.vue
@@ -1,7 +1,8 @@
 <template>
 	<div class="admin-new-post-page">
 		<section class="new-post-form">
-			<form>
+			<!-- prevent is a standard vue modifier -->
+			<form @submit.prevent="onSave">
 				<AppControlInput v-model="editedPost.author">Author Name</AppControlInput>
 				<AppControlInput v-model="editedPost.title">Title</AppControlInput>	
 				<AppControlInput v-model="editedPost.thumbnailLink">Thumbnail Link</AppControlInput>	
@@ -22,3 +23,35 @@
 		</section>
 	</div>
 </template>
+
+<script>
+import AppControlInput from '@/components/UI/AppControlInput'
+import AppButton from '@/components/UI/AppButton'
+
+export default {
+	components: {
+		AppControlInput,
+		AppButton
+	},
+	data() {
+		return {
+			editedPost: {
+				author: '',
+				title: '',
+				thumbnailLink: '',
+				content: ''
+			}
+		}
+	},
+	methods: {
+		onSave() {
+			//Save the post
+			console.log(this.editedPost);
+		},
+		onCancel() {
+			this.$router.push('/admin');
+			//Navigate back
+		}
+	}
+}
+</script>
