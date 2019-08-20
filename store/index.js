b19a8c83114f5fae3c9ba4291719b9bb28d2ed9c
artseven <yabkevich.artsiom.09@gmail.com>
2019-08-20 11:07:10 -0400

    adding store mutations

--- store/index.js
+++ store/index.js
@@ -13,23 +13,44 @@ const createStore = () => {
     mutations: {
       setPosts (state, posts) {
         state.loadedPosts = posts
+      },
+      addPost (state, post) {
+        state.loadedPosts.push(post)
+      },
+      editPost (state, editedPost) {
+        const postIndex = state.loadedPosts.findIndex(post => post.id === editedPost.id);
+        state.loadedPosts[postIndex] = editedPost
       }
     },
     actions: {
-      	nuxtServerInit (vuexContext, context) {
-			return axios.get('https://nuxt-blog-c63d2.firebaseio.com/posts.json')
-				.then(res => {
-					const postsArray = []
-					for (const key in res.data) {
-						postsArray.push({ ...res.data[key], id: key })
-					}
-					vuexContext.commit('setPosts', postsArray)
-				})
-				.catch(e => context.error(e));
+      nuxtServerInit (vuexContext, context) {
+			  return axios.get('https://nuxt-blog-c63d2.firebaseio.com/posts.json')
+          .then((res) => {
+            const postsArray = []
+            for (const key in res.data) {
+              postsArray.push({ ...res.data[key], id: key })
+            }
+            vuexContext.commit('setPosts', postsArray)
+          })
+          .catch(e => context.error(e));
 		},
 		setPosts (vuexContext, posts) {
 			vuexContext.commit('setPosts', posts)
-		}
+    },
+    addPost (vuexContext, post) {
+      const createdPost = {
+        ...postData,
+        updatedDate: new Date()
+      }
+      axios.post('https://nuxt-blog-c63d2.firebaseio.com/posts.json', createdPost)
+				.then(result => {
+          vuexContext.commit('addPost', { ...createdPost })
+          this.$router.push('/admin')
+				})
+				.catch(e => console.log(e))
+    },
+    editPost (vuexContext, editedPost) {
+    }
     },
     getters: {
       loadedPosts (state) {
